eb6a07ee9e221756b559e95f63095eff9172e696
PrestonhahaYT <prestonc.ects@gmail.com>
2024-10-10 14:20:16 -0400

    ALL OF MY STUFF MAyBE

---
+++ Control Flow/the js.js
@@ -0,0 +1,5 @@
+let principal = 1000;
+
+let interestRate = 0.05;
+
+let years = 5;
---
+++ OBJECTS/built.js
@@ -0,0 +1,14 @@
+let randomNum = Math.random();
+console.log(randomNum);
+// Upper bound is not incluted
+function getRandomInteger(max) {
+  return math.floor(Math.random() * max + 1);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(getRandomInteger(10));
+}
+
+// date is another built object
+const now = new Date("Feb 15 2000");
+console.log("it is ${Now} a day of the year");
